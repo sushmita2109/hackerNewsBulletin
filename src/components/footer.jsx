ca3a6a2330db02aeb74e47afcb9259fbe5f845e6
Sushmita <sushmitakumari363@gmail.com>
2022-12-28 10:20:04 +0530

    sites opening in blank pages

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,36 +1,40 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faYoutube,
-    faFacebook,
-    faTwitter,
-    faInstagram
-  } from "@fortawesome/free-brands-svg-icons";
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 import './styles/footer.css';
 
-const Footer=()=>{
-return(
+const Footer = () => {
+  return (
     <div className="footer">
-        <div className="footerHeader">
-            Follow us on
-        </div>
-        <a href="https://www.youtube.com/c/thehackernews"
+      <div className="footerHeader">
+        Follow us on
+      </div>
+      <a href="https://www.youtube.com/c/thehackernews"
+        target="_blank"
         className="youtubesocial">
         <FontAwesomeIcon color="white" icon={faYoutube} size="2x" />
       </a>
       <a href="https://www.facebook.com/thehackernews/"
+        target="_blank"
         className="facebooksocial">
         <FontAwesomeIcon color="white" icon={faFacebook} size="2x" />
       </a>
-      <a href="https://twitter.com/TheHackersNews?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" className="twittersocial">
+      <a href="https://twitter.com/TheHackersNews?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+        target="_blank" className="twittersocial">
         <FontAwesomeIcon color="white" icon={faTwitter} size="2x" />
       </a>
       <a href="https://www.instagram.com/thehackernews/?hl=en"
+        target="_blank"
         className="instagramsocial">
         <FontAwesomeIcon color="white" icon={faInstagram} size="2x" />
       </a>
     </div>
-)
+  )
 }
 
 export default Footer;
